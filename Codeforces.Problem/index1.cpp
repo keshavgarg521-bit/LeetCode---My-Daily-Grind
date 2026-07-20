301f62425f826b2d77788e8fdf1e1a52254d07f5
keshav garg <keshavgarg521@gmail.com>
2026-07-20 23:16:45 +0530

    today'sprogress

--- Codeforces.Problem/index1.cpp
+++ Codeforces.Problem/index1.cpp
@@ -250,3 +250,31 @@
 //   }
 // }
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        unordered_map<char,long> left;
+        unordered_map<char,int> right;
+        for(int i = 0 ; i<s.size() ; i++){
+            right[s[i]]++;
+        }
+        int maxi = INT_MIN;
+        for(int i = 0 ; i<s.size() ; i++){
+            left[s[i]]++;
+            right[s[i]]--;
+            if(right[s[i]] == 0){
+                right.erase(s[i]);
+            }
+            int sum = left.size() + right.size();
+            maxi = max(maxi,sum);
+        }
+        cout<<maxi<<endl;
+    }
+}
